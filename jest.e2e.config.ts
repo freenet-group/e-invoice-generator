eb43b77fb07f35d689cafb55b983b70d62c24180
Stefan Peppe <Stefan.Peppe@md.de>
2026-03-06 13:58:56 +0100

    integration test moved to integration

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '.',
-  roots: ['<rootDir>/test/e2e'],
+  roots: ['<rootDir>/test/integration'],
   testMatch: ['**/*.e2e.test.ts'],
   testTimeout: 120_000,
   verbose: true,
