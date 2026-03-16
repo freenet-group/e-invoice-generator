eb0354acec75b20e1cba711ba896a539c3200622
Stefan Peppe <Stefan.Peppe@md.de>
2026-03-16 12:15:36 +0100

    service name from environment

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -3,7 +3,7 @@ import pino from 'pino'
 export const logger = pino({
     level: process.env['LOG_LEVEL'] ?? 'info',
     base: {
-        service: 'mcbs-zugferd-converter',
+        service: process.env['SERVICE_NAME'] ?? 'e-invoice-generator',
         env: process.env['STAGE'] ?? process.env['NODE_ENV'] ?? 'unknown'
     }
 })
